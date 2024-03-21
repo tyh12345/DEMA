a4a1f81dbdf9e9f6233e500ed4abd67312f226ca
tyh12345 <97788490+tyh12345@users.noreply.github.com>
2024-03-21 12:38:10 +0800

    Update ICP_SAM.py

--- ICP_SAM.py
+++ ICP_SAM.py
@@ -50,7 +50,7 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 sys.path.append("..")
 
-sam_checkpoint = "./sam_vit_h_4b8939.pth"
+sam_checkpoint = "***"
 model_type = "vit_h"
 
 device = "cuda"
@@ -129,26 +129,26 @@ def show_con(anns,img,blob=False):
     cv2.waitKey(0)
     cv2.imshow('img', img2)
     cv2.waitKey(0)
-    cv2.imwrite('./alldata/r1.png',img1)
-    cv2.imwrite('./alldata/r2.png', img2)
+    cv2.imwrite('***',img1)
+    cv2.imwrite('***', img2)
 
 
 
 mask_generator_2 = SamAutomaticMaskGenerator(
     model=sam,
-    points_per_side=50,
-    pred_iou_thresh=0.86,
-    stability_score_thresh=0.88,
-    crop_n_layers=1,
-    crop_n_points_downscale_factor=2,
-    min_mask_region_area=100,  # Requires open-cv to run post-processing
+    points_per_side=***,
+    pred_iou_thresh=***,
+    stability_score_thresh=***,
+    crop_n_layers=***,
+    crop_n_points_downscale_factor=***,
+    min_mask_region_area=***,  # Requires open-cv to run post-processing
 )
 
 masks2 = mask_generator_2.generate(image)
 
 
 
-newmask  = list(filter(lambda x:x['area']>2000 and x['area'] < 20000,masks2))
+newmask  = list(filter(lambda x:x['area']>*** and x['area']<***,masks2))
 plt.figure(figsize=(20,20))
 plt.imshow(image)
 show_anns(newmask)
